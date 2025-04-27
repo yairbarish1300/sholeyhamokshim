7efe85447e1240382a5bee9892f5302ccbc216fe
RichardKes <pinikes12@gmail.com>
2025-04-27 15:41:56 +0300

    added-functions

--- Sh.js
+++ Sh.js
@@ -9,7 +9,12 @@ class Shoola {
 
     }
     checkWin() {
-
+        for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < 10; j++) {
+                if (this.board[i][j] == 1) return false;
+            }
+        }
+        return true;
     }
     init(whith, height, boombsNum) {
         this.board = [];
@@ -38,12 +43,20 @@ class Shoola {
         this.win = true
 
     }
-    calculateMine() {
-
+    calculateMine(x, y) {
+        let sum = 0;
+        for (let i = x - 1; i <= x + 1; i++) {
+            for (let j = y - 1; j <= y + 1; j++) {
+                if (i >= 0 && j >= 0 && i < 10 && j < 10 && this.board[i][j] < 0) sum++;
+            }
+        }
+        return Math.abs(sum);
     }
-
-
-
-
-
+    reveal(x, y) {
+        for (let i = x - 1; i <= x + 1; i++) {
+            for (let j = y - 1; j <= y + 1; j++) {
+                if (this.board[i][j] == 1) this.clickCell(i, j);
+            }
+        }
+    }
 }
