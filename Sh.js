6e6104891084c60e7993044202891bfad7658e36
RichardKes <pinikes12@gmail.com>
2025-04-27 14:28:04 +0300

    Update Sh.js

--- Sh.js
+++ Sh.js
@@ -1,25 +1,45 @@
-class Shoola{
-    board=[];
-    Boombs;
-    active=false;
-    constructor(){
+class Shoola {
+    board = [];
+    active = false;
+    win = false;
+    constructor() {
 
     }
-    clickCell(){
+    clickCell() {
 
     }
-    checkWin(){
+    checkWin() {
 
     }
-    init(whith,height,boombsNum){
-           for (let i = 0; i < ; i++) {
-            const element = array[i];
-            
-           }
-        
+    init(whith, height, boombsNum) {
+        this.board = [];
+        for (let i = 0; i < 10; i++) {
+            this.board.push(new Array(10).fill(0)) // creat 2 dimensions Array
+
+        }
+        for (let i = 0; i < boombsNum; i++) {
+            let rndA = Math.floor(Math.random() * 10)
+            let rndB = Math.floor(Math.random() * 10)
+            if (!(this.board[rndA][rndB])) {
+                this.board[rndA][rndB] = -1;
+            }
+            else {
+                i--;
+            }
+        }
+        for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < 10; j++) {
+                if (this.board[i][j] != -1) {
+                    this.board[i][j] == 1
+                }
+            }
+        }
+        this.active = true
+        this.win = true
+
     }
-    calculateMine(){
-        
+    calculateMine() {
+
     }
 
 
