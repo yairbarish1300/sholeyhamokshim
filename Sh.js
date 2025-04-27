0e6bb5dca5a0fc0dad0ec5b3da789098a291b678
RichardKes <pinikes12@gmail.com>
2025-04-27 13:56:49 +0300

    Add files via upload

---
+++ Sh.js
@@ -0,0 +1,29 @@
+class Shoola{
+    board=[];
+    Boombs;
+    active=false;
+    constructor(){
+
+    }
+    clickCell(){
+
+    }
+    checkWin(){
+
+    }
+    init(whith,height,boombsNum){
+           for (let i = 0; i < ; i++) {
+            const element = array[i];
+            
+           }
+        
+    }
+    calculateMine(){
+        
+    }
+
+
+
+
+
+}
