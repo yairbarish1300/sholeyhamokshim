2855e6f700daede96b5e2e98ea96d55ef25334a5
davidbrodsky058 <dr.brodsky058756@gmail.com>
2025-05-04 10:42:07 +0300

    arranged funcs. created obj and initialized

--- Sh.js
+++ Sh.js
@@ -2,25 +2,29 @@ class Shoola {
   board = [];
   active = false;
   win = false;
-  constructor() {}
-  clickCell(i, j) {
-    if (this.checkWin) {
-      this.active = false;
-      this.win = true;
-      renderWin();
-    } else {
-      this.board[i][j] *= 2;
-      if (this.board[i][j] == -2) {
-        this.renderLoss();
-        this.active = false;
-      } else if (this.board[i][j] == 2) {
-        let numOfBombs = this.calculateMine(i, j);
-        showCell(numOfBombs, this.board[i][j]);
-        // const cell = document.querySelector(`.cell[data-i="${i}"][data-j="${j}"]`);
-        // if (numOfBombs!=0)cell.innerHTML=numOfBombs;
-        if (numOfBombs == 0) this.reveal();
+  constructor() {
+    this.board = [];
+    for (let i = 0; i < 10; i++) {
+      this.board.push(new Array(10).fill(0)); // create 2 dimensions Array
+    }
+    for (let i = 0; i < boombsNum; i++) {
+      let rndA = Math.floor(Math.random() * 10);
+      let rndB = Math.floor(Math.random() * 10);
+      if (!this.board[rndA][rndB]) {
+        this.board[rndA][rndB] = -1;
+      } else {
+        i--;
       }
     }
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        if (this.board[i][j] != -1) {
+          this.board[i][j] == 1;
+        }
+      }
+    }
+    this.active = true;
+    this.win = false;
   }
   checkWin() {
     for (let i = 0; i < 10; i++) {
@@ -53,6 +57,7 @@ class Shoola {
     }
     this.active = true;
     this.win = false;
+    render();
   }
   calculateMine(x, y) {
     let sum = 0;
@@ -70,4 +75,26 @@ class Shoola {
       }
     }
   }
+  clickCell(i, j) {
+    if (this.checkWin) {
+      this.active = false;
+      this.win = true;
+      renderWin();
+    } else {
+      this.board[i][j] *= 2;
+      if (this.board[i][j] == -2) {
+        this.renderLoss();
+        this.active = false;
+        this.win=false;
+      } else if (this.board[i][j] == 2) {
+        let numOfBombs = this.calculateMine(i, j);
+        showCell(numOfBombs, this.board[i][j]);
+        // const cell = document.querySelector(`.cell[data-i="${i}"][data-j="${j}"]`);
+        // if (numOfBombs!=0)cell.innerHTML=numOfBombs;
+        if (numOfBombs == 0) this.reveal();
+      }
+    }
+  }
 }
+const shoolaGame=new Shoola();
+shoolaGame.init(0,0,20);
